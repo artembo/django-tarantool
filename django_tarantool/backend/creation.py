75ffd05085c1f1e54f4abab5e700b08161e19ba3
artem.morozov <artembo@me.com>
2020-03-05 18:55:42 +0300

    db creation initial

--- django_tarantool/backend/creation.py
+++ django_tarantool/backend/creation.py
@@ -1,19 +1,46 @@
+import os
+import shutil
+import subprocess
+import time
+from pathlib import Path
+
+from django.core.management import call_command
 from django.db.backends.base.creation import BaseDatabaseCreation
+
+from django_app import settings
 
 
 class DatabaseCreation(BaseDatabaseCreation):
+    tarantool_process = None
+    db_path = None
 
     def _quote_name(self, name):
         return self.connection.ops.quote_name(name)
 
-    def _get_database_create_suffix(self, encoding=None, template=None):
-        raise NotImplementedError
+    def _prepare_db_path(self, db_name):
+        path = os.path.join(settings.BASE_DIR, db_name)
+        Path(path).mkdir(parents=True, exist_ok=True)
+        return path
 
-    def sql_table_creation_suffix(self):
-        raise NotImplementedError
+    def start_test_tarantool(self):
+        test_database_name = self._get_test_db_name()
+        self.db_path = self._prepare_db_path(test_database_name)
+        self.tarantool_process = subprocess.Popen(['tarantool', 'start.lua'], cwd=self.db_path)
+        time.sleep(1)
 
-    def _database_exists(self, cursor, database_name):
-        raise NotImplementedError
+    def create_test_db(self, verbosity=1, autoclobber=False, serialize=True, keepdb=False):
+        settings.DATABASES[self.connection.alias]["PORT"] = 3302
+        self.connection.settings_dict["PORT"] = 3302
+        self.start_test_tarantool()
 
-    def _clone_test_db(self, suffix, verbosity, keepdb=False):
-        raise NotImplementedError
+        call_command('migrate', interactive=False)
+
+    def destroy_test_db(self, old_database_name=None, verbosity=1, keepdb=False, suffix=None):
+        super().destroy_test_db(old_database_name, verbosity, keepdb, suffix)
+        self.tarantool_process.kill()
+
+    def _destroy_test_db(self, test_database_name, verbosity):
+        print(test_database_name)
+        print(self.db_path)
+        # shutil.rmtree()
+
