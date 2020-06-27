8ebafd13698e4fd471272f032fcaa6d68376730f
artem.morozov <artembo@me.com>
2020-06-27 13:10:23 +0300

    remove unique index

--- django_tarantool/backend/features.py
+++ django_tarantool/backend/features.py
@@ -4,6 +4,7 @@ from django.utils.functional import cached_property
 
 class DatabaseFeatures(BaseDatabaseFeatures):
     uses_savepoints = False
+    can_alter_table_rename_column = False
 
     @cached_property
     def supports_transactions(self):
